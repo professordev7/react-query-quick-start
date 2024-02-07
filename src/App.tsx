d701c011ead659958dcbf8d0a42860f4a6f01b01
Salar <Salar.Farahmand@infos.it>
2024-02-08 00:14:39 +0100

    Added react-query useQuery, useMutation examples

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,64 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useMutation, useQuery } from "@tanstack/react-query";
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const userData = useQuery({
+    queryKey: ["users"],
+    queryFn: () => {
+      return fetch("https://jsonplaceholder.typicode.com/users").then(
+        (response) => response.json()
+      );
+    },
+  });
+
+  const mutatePost = useMutation({
+    mutationKey: ["posts"],
+    mutationFn: (newPost: any) => {
+      return fetch("https://jsonplaceholder.typicode.com/posts", {
+        method: "POST",
+        body: JSON.stringify(newPost),
+        headers: {
+          "Content-type": "application/json; charset=UTF-8",
+        },
+      }).then((response) => response.json());
+    },
+  });
 
   return (
     <>
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+        <button onClick={() => userData.refetch()}>Get Users</button>
+        <div>
+          {userData.isFetching && <div>Fetching user data...</div>}
+          {userData.isError && <div>{`Error get data!!!`}</div>}
+          {userData.data &&
+            userData.data.length > 0 &&
+            userData.data.map((user: any) => <div>{user.name}</div>)}
+        </div>
+        <hr />
+        <div>
+          <button
+            onClick={() =>
+              mutatePost.mutate({
+                title: "First Post",
+                body: "First Post Body",
+                userId: 1,
+              })
+            }
+          >
+            Add New Post
+          </button>
+          <div>
+            {mutatePost.isPending && <div>Adding new post...</div>}
+            {mutatePost.isError && <div>{`Error add new post!!!`}</div>}
+            {mutatePost.data && (
+              <div>{`Success add new post with title : '${mutatePost.data.title}'`}</div>
+            )}
+          </div>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
